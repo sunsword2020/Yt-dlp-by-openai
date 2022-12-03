26694959d6ad618cab9f7a46930736962fb3c9f7
sunsword2020 <73904317+sunsword2020@users.noreply.github.com>
2022-12-04 01:22:20 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,4 +6,4 @@ from telegram.utils.request import Request
 def get_bot():
     # Default size from the library, 4 workers + 4 additional
     request = Request(con_pool_size=8)
-    return Bot(token=os.getenv("TG_TOKEN"), request=request)
+    return Bot(token=os.getenv("TELEGRAM_BOT_TOKEN"), request=request)
